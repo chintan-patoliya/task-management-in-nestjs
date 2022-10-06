e3e06b89e166a46f29e33cc3df8b86f566f15185
chintan-patoliya <chintanpatoliya111@gmail.com>
2022-10-06 18:39:49 +0530

    going live

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { configValidationSchema } from './config.schema';
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: [`.env.stage.${process.env.STAGE}`],
+      // envFilePath: "prod.env",
+
+      // envFilePath: [`.env.stage.${process.env.NODE_ENV}`],
+
       // envFilePath:".env.stage.prod",
       validationSchema: configValidationSchema,
     }),
     TasksModule,
     TypeOrmModule.forRootAsync({
+// require("dotenv").config(),
+
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => {
